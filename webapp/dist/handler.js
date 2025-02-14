a9c9a729d6e5d0c4caf2147fc2dced81284188a9
thomas <thomas.mclaughlin2@stvincent.edu>
2025-02-14 04:35:10 -0500

    working iteration

--- webapp/dist/handler.js
+++ webapp/dist/handler.js
@@ -5,19 +5,47 @@
 // HTTP responses are created using the ServerResponse object.
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.handler = void 0;
-const fs_1 = require("fs");
+const fs = require('fs');
+const path = require('path');
 const handler = (req, res) => {
-    //readFile reads contents of data.json
-    (0, fs_1.readFile)("data.json", (err, data) => {
-        if (err == null) {
-            res.end(data, () => console.log("Filesent"));
-            // callback here logs FileSent after sending data to screen
+    if (req.url?.startsWith('/images/')) {
+        const imagePath = path.join(__dirname, req.url); // appending
+        console.log(imagePath); // For debugging
+        console.log(req.url); // For debugging
+        fs.readFile(imagePath, (error, data) => {
+            // console.log(typeof data);
+            if (error) {
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.end('Image not found.');
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+            res.end(data);
+        });
+        return;
+    }
+    fs.readFile('dist/index.html', function (error, data) {
+        // console.log(typeof data); Documentation says this should be a buffer but says it is an object
+        if (error) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.write('Error!');
+            return res.end();
         }
-        else {
-            console.log(`Error: ${err.message}`);
-            res.statusCode = 500;
-            res.end();
-        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        res.end();
     });
+    //readFile reads contents of data.json
+    // fs.readFile("data.json", (err: Error | null, data: Buffer) => {
+    //     if (err == null) {
+    //         res.end(data, () => console.log("Filesent"));
+    //         // callback here logs FileSent after sending data to screen
+    //     }
+    //     else {
+    //         console.log(`Error: ${err.message}`);
+    //         res.statusCode = 500;
+    //         res.end();
+    //     }
+    // });
 };
 exports.handler = handler;
--- webapp/src/handler.ts
+++ webapp/src/handler.ts
@@ -5,18 +5,51 @@
 
 import { IncomingMessage, ServerResponse } from "http";
 const fs= require('fs');
+const path= require('path');
 
 export const handler = (req: IncomingMessage, res:ServerResponse) => {
-    //readFile reads contents of data.json
-    fs.readFile("data.json", (err: Error | null, data: Buffer) => {
-        if (err == null) {
-            res.end(data, () => console.log("Filesent"));
-            // callback here logs FileSent after sending data to screen
+    
+    if(req.url?.startsWith('/images/')){
+        const imagePath= path.join(__dirname, req.url); // appending
+        console.log(imagePath); // For debugging
+        console.log(req.url); // For debugging
+        fs.readFile(imagePath, (error: any, data: any)=>{
+            // console.log(typeof data);
+            if (error){
+                res.writeHead(404, {'Content-Type': 'text/plain'});
+                res.end('Image not found.');
+                return;
+            }
+            res.writeHead(200, {'Content-Type': 'image/jpeg'});
+            res.end(data);
+        });
+        return;
+    }
+
+    fs.readFile('dist/index.html', function(error: any, data: Buffer){
+        // console.log(typeof data); Documentation says this should be a buffer but says it is an object
+
+        if(error){
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.write('Error!');
+            return res.end();
         }
-        else {
-            console.log(`Error: ${err.message}`);
-            res.statusCode = 500;
-            res.end();
-        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+        
     });
+
+    //readFile reads contents of data.json
+    // fs.readFile("data.json", (err: Error | null, data: Buffer) => {
+    //     if (err == null) {
+    //         res.end(data, () => console.log("Filesent"));
+    //         // callback here logs FileSent after sending data to screen
+    //     }
+    //     else {
+    //         console.log(`Error: ${err.message}`);
+    //         res.statusCode = 500;
+    //         res.end();
+    //     }
+    // });
 };
