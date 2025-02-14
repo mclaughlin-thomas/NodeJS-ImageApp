b235d24a55b2daa7e2c01b0425d1a4805d03d5c4
thomas <thomas.mclaughlin2@stvincent.edu>
2025-02-13 23:50:48 -0500

    added documentation handler.ts

--- webapp/src/handler.ts
+++ webapp/src/handler.ts
@@ -1,5 +1,7 @@
 // Thomas McLaughlin
 // This code will process the HTTP requests.
+// HTTP requests are represented by Incoming Message object.
+// HTTP responses are created usint the ServerResponse object.
 
 import { IncomingMessage, ServerResponse } from "http";
 export const handler = (req: IncomingMessage, res: ServerResponse) => {
