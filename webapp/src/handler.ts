385460c5ca1a619fa2ddc56362d48b7ab3299810
thomas <thomas.mclaughlin2@stvincent.edu>
2025-02-13 23:49:09 -0500

    filled handler.ts

--- webapp/src/handler.ts
+++ webapp/src/handler.ts
@@ -1,3 +1,7 @@
 // Thomas McLaughlin
 // This code will process the HTTP requests.
 
+import { IncomingMessage, ServerResponse } from "http";
+export const handler = (req: IncomingMessage, res: ServerResponse) => {
+    res.end("Hello World");
+};
