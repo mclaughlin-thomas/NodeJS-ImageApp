2d609cb4c00dfe327095eb97b77f965535077f1c
thomas <thomas.mclaughlin2@stvincent.edu>
2025-02-13 23:54:39 -0500

    added documentation server.ts

--- webapp/src/server.ts
+++ webapp/src/server.ts
@@ -1,9 +1,12 @@
 // Thomas McLaughlin
+// This creates a simple HTTP server that listens for
+// HTTP requests on port 8000, and processes them using
+// the function defined in the handler.ts file.
 
 import { createServer } from "http";
 import { handler } from "./handler";
 
-const port = 5000;
+const port = 8000;
 
 const server = createServer(handler);
 server.listen(port, function() {
