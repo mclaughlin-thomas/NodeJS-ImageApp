80735aec3f4c7864c41088acfab4300c2d5bac40
thomas <thomas.mclaughlin2@stvincent.edu>
2025-02-15 22:47:39 -0500

    updated handler.ts and handler.js

--- webapp/dist/handler.js
+++ webapp/dist/handler.js
@@ -9,13 +9,14 @@ const fs = require('fs');
 const path = require('path');
 const handler = (req, res) => {
     if (req.url?.startsWith('/images/')) {
+        //Our images have requests as well!
         console.log(req.url); // For debugging. This is the vanilla url of the request, i.e., /images/auspicious.jpg
         const imagePath = path.join(__dirname, req.url); // appending
         console.log(imagePath); // For debugging. This is the entire path, /home/thomas/Documents/Node/ImageApp/webapp/dist/images/auspicious.jpg
         fs.readFile(imagePath, (error, data) => {
             if (error) {
                 res.writeHead(404, { 'Content-Type': 'text/plain' });
-                res.end('Image not found!');
+                res.end('Not Found...');
                 return;
             }
             // all images are jpeg
@@ -25,6 +26,7 @@ const handler = (req, res) => {
         return;
     }
     fs.readFile('dist/index.html', function (error, data) {
+        // Using dist/index.html because this logic ran from dist directory
         if (error) {
             res.writeHead(500, { 'Content-Type': 'text/plain' });
             res.write('Error!');
--- webapp/src/handler.ts
+++ webapp/src/handler.ts
@@ -3,7 +3,12 @@
 // HTTP requests are represented by Incoming Message object.
 // HTTP responses are created using the ServerResponse object.
 
-
+// This largely uses the same setup as seen in our book Mastering Node.js
+// with some reference to outside resources to serve html and jpeg files.
+// The book does a good job of explaining TypeScript and JavaScript topics,
+// but from the sections read so far they did not explicitly cover how to 
+// handle requests for files. Aside from referencing our book I watched
+// this tutorial on serving a 
 
 
 import { IncomingMessage, ServerResponse } from "http";
@@ -13,13 +18,15 @@ const path= require('path');
 export const handler = (req: IncomingMessage, res:ServerResponse) => {
     
     if(req.url?.startsWith('/images/')){
+        //Our images have requests as well!
+
         console.log(req.url); // For debugging. This is the vanilla url of the request, i.e., /images/auspicious.jpg
         const imagePath= path.join(__dirname, req.url); // appending
         console.log(imagePath); // For debugging. This is the entire path, /home/thomas/Documents/Node/ImageApp/webapp/dist/images/auspicious.jpg
         fs.readFile(imagePath, (error: any, data: any)=>{
             if (error){
                 res.writeHead(404, {'Content-Type': 'text/plain'});
-                res.end('Image not found!');
+                res.end('Not Found...');
                 return;
             }
             // all images are jpeg
@@ -28,9 +35,10 @@ export const handler = (req: IncomingMessage, res:ServerResponse) => {
         });
         return;
     }
-
+    
     fs.readFile('dist/index.html', function(error: any, data: Buffer){
-
+        // Using dist/index.html because this logic ran from dist directory
+        
         if(error){
             res.writeHead(500, {'Content-Type': 'text/plain'});
             res.write('Error!');
